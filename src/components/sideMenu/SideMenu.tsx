b71200a50b03020a0790471ac0e8eb797c1efc37
José Gabriel <89746639+juserrrrr@users.noreply.github.com>
2022-10-14 19:08:13 -0300

    up: update no AppBar e no Menu

--- src/components/sideMenu/SideMenu.tsx
+++ src/components/sideMenu/SideMenu.tsx
@@ -1,69 +1,73 @@
-import { Bookmark, Home } from "@mui/icons-material";
-import { AppBar, Box, CssBaseline, Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText, Typography, useTheme, Toolbar } from "@mui/material";
+import { AccountCircle, Bookmark, Close, Home, Menu } from "@mui/icons-material";
+import { AppBar, Box, CssBaseline, Divider, Drawer, List, ListItemButton, ListItemIcon, ListItemText, Typography, useTheme, Toolbar, IconButton } from "@mui/material";
+import zIndex from "@mui/material/styles/zIndex";
 import React, { useState } from "react";
 
 function SideMenu () {
-  const [isOpen,setIsOpen] = useState<boolean>(true);
+  const [isOpen,setIsOpen] = useState<boolean>(false);
 
   return (
     <>
-      <AppBar position="fixed" color="primary">
-        <Toolbar>
-          <Typography variant="h6">
-            
-          </Typography>
-        </Toolbar>
-      </AppBar>
-      {/* MENU */}
-      <Drawer
-        variant="permanent"
-        anchor="left"
-        open={isOpen}
-        onClose={()=>{setIsOpen(false)}}
-      >
-        {/* ALL MENU BOX  */}
-        <Box
-          bgcolor='primary.main'
-          sx={{
-            width:'250px',
-            height:'100vh',
-            display:'flex',
-            flexDirection:'column',
-          }}
-        >
-          {/* HEADER BOX */}
-          <Box
-          sx={{
-            width:'100%',
-            height:'10%',
-            display:'flex',
-            alignItems:'center',
-            justifyContent:'center'
-          }}
-          >
+      <Box sx={{display:'flex',bgcolor:'black'}}>    
+        <AppBar position="fixed" color="primary" sx={{zIndex:zIndex.drawer+1}}>
+          <Toolbar>
+            <IconButton
+            size="large"
+            edge='start'
+            onClick={()=>{setIsOpen(!isOpen)}}
+            >
+              {isOpen ? <Close /> : <Menu/>}
+            </IconButton>
+
+
             <Bookmark fontSize="large"/>
-            <Typography variant="h6" component='div'>
+            <Typography variant="h6" component='div' sx={{flexGrow:1}} >
               SysBiblioteca
             </Typography>
-          </Box>
 
-          <Divider />
-          {/* CONTENT BOX */}
-          <Box flex={1}>
-            <List component='nav'>
-              <ListItemButton>
-                <ListItemIcon>
-                  <Home/>
-                </ListItemIcon>
-                <ListItemText primary='Pagina Inicial'/>
-              </ListItemButton>
-            </List>
+            <IconButton
+            size="large"
+            edge='start'
+            >
+              <AccountCircle fontSize="large"/>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+        {/* MENU */}
+        <Drawer
+          variant="temporary"
+          anchor="left"
+          open={isOpen}
+          onClose={()=>{setIsOpen(false)}}
+          >
+          {/* ALL MENU BOX  */}
+          <Box
+            bgcolor='primary.main'
+            sx={{
+              width:'250px',
+              height:'100vh',
+              display:'flex',
+              flexDirection:'column',
+              position:'relative',
+            }}
+            >
+            {/* CONTENT BOX */}
+            <Box flex={1}>
+              <List component='nav'>
+                <ListItemButton>
+                  <ListItemIcon>
+                    <Home/>
+                  </ListItemIcon>
+                  <ListItemText primary='Pagina Inicial'/>
+                </ListItemButton>
+              </List>
+            </Box>
+            
+            
           </Box>
           
-          
-        </Box>
-        
-      </Drawer>
+        </Drawer>
+      </Box>
     </>
   )
   
